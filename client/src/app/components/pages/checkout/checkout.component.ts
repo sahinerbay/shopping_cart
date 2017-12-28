55615506e15924b3e1b6ac239ce4e30812ea338d
sahinerbay <sahin@sahinerbay.com>
2017-12-28 12:44:15 +0200

    Use shared shopping-cart components in checkout page

--- client/src/app/components/pages/checkout/checkout.component.ts
+++ client/src/app/components/pages/checkout/checkout.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartStateService } from './../../../_services/shopping-cart/shopping-cart-state.service';
+import { ShoppingCart } from './../../../_classes/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+	selector: 'app-checkout',
+	templateUrl: './checkout.component.html',
+	styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	constructor(private shoppingCartStateService: ShoppingCartStateService) { }
 
-  ngOnInit() {
-  }
+	private shoppingCart: ShoppingCart;
+	private shoppingCartSubscription: Subscription;
+
+	ngOnInit() {
+		this.shoppingCartSubscription = this.loadShoppingCart();
+	}
+
+	loadShoppingCart(): Subscription {
+		return this.shoppingCartStateService.getState()
+			.subscribe((shoppingCart: ShoppingCart) => this.shoppingCart = shoppingCart);
+	}
+
+	isShoppingCartEmpty(shoppingCart: ShoppingCart): boolean {
+		if (shoppingCart.items.length !== 0) {
+			return true;
+		}
+		return false;
+	}
+
+	ngOnDestroy() {
+		this.shoppingCartSubscription.unsubscribe();
+	}
 
 }
